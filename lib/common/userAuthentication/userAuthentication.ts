13c4a3b5a6ee4077557b91bd7fa7cb4c6a3609de
Zain Munir <zainmunir4112@gmail.com>
2022-08-10 14:55:44 +0500

    Some bug fixes and improved dummy data

--- lib/common/userAuthentication/userAuthentication.ts
+++ lib/common/userAuthentication/userAuthentication.ts
@@ -4,49 +4,37 @@ import { UserService } from "../../modules/Users/services/user.services";
 import errorHandler from '../handler/error.handler';
 import mongoose from "mongoose";
 
-interface IRequest extends Request{
-    user:any
+interface IRequest extends Request {
+    user: any
 }
 
-export default async(req: IRequest, res: Response, next: any) => {
-    try{
+export default async (req: IRequest, res: Response, next: any) => {
+    try {
         const token: any = req.headers['x-access-token'];
-        if (!token){
-            throw{
+        if (!token) {
+            throw {
                 status: 401,
                 message: 'No token provided.'
             }
         }
-          
-        jwt.verify(token, process.env.JWT_SECRET, async function(err: any, decoded: any) {
-          if (err){
-              throw{
-                  status: 403,
-                  message: 'Failed to authenticate token.'
-              }
-          }
-          
-          //Verify if decoded userId is valid userId
-          const user: any = new UserService();
-          const getUser: any = await user.fetchUser({ _id: new mongoose.Types.ObjectId(decoded.uid) });
-          if(!(getUser.success)){
-              throw{
-                  status: 404,
-                  message: "User not found"
-              }
-          }
 
-          req.user = {
-                user_id: getUser.data._id.toString(),
-                email: getUser.data.email,
-                displayName: getUser.data.displayName
-            };
+        const decoded = await jwt.verify(token, process.env.JWT_SECRET)
 
-            next();
-        });
+        //Verify if decoded userId is valid userId
+        const user: any = new UserService();
+        const getUser: any = await user.fetchUser({ _id: new mongoose.Types.ObjectId(decoded.uid) });
+        if (!(getUser.success)) throw getUser
 
-    }catch(error: any){
-        if(error.name === 'JsonWebTokenError'){
+        req.user = {
+            user_id: getUser.data._id.toString(),
+            email: getUser.data.email,
+            displayName: getUser.data.displayName
+        };
+
+        next();
+
+    } catch (error: any) {
+        if (error.name === 'JsonWebTokenError') {
             error.message = 'Authentication failed'
         }
 
